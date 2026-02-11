537b01b9801b8e4a921824f85d1f21b749a46f63
nidarshanamohankumar-coder <nidarshanamohankumar@gmail.com>
2026-02-11 10:53:36 +0530

    Add files via upload

--- db.php
+++ db.php
@@ -1,15 +1,7 @@
 <?php
-$host = "dpg-d65chdd6ubrc73912ojg-a";
-$dbname = "register_9hrv";
-$user = "users";
-$password = "XEsVvJGs4RzC6K7RGYO54p7N2ei5JXRW";
-$port = "5432";
-
-$conn = pg_connect(
-    "host=$host port=$port dbname=$dbname user=$user password=$password"
-);
+$conn = pg_connect("host=ep-bitter-king-aiocqno2-pooler.c-4.us-east-1.aws.neon.tech port=5432 dbname=neondb user=neondb_owner password=psql 'postgresql://neondb_owner:npg_qsL2NcjP3roS@ep-bitter-king-aiocqno2-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require' sslmode=require");
 
 if (!$conn) {
-    die("Database connection failed");
+    die("Database connection failed!");
 }
 ?>
