c0de4b6c8d099c257ae6dceb0cdd8119a9b5f359
nidarshanamohankumar-coder <nidarshanamohankumar@gmail.com>
2026-02-11 11:19:37 +0530

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = pg_connect("host=ep-bitter-king-aiocqno2-pooler.c-4.us-east-1.aws.neon.tech port=5432 dbname=neondb user=neondb_owner password=psql 'postgresql://neondb_owner:npg_qsL2NcjP3roS@ep-bitter-king-aiocqno2-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require' sslmode=require");
-
-if (!$conn) {
-    die("Database connection failed!");
-}
-?>
