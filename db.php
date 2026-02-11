56cb8cbe350dab09455c0ef659bf4368f52a2fd2
nidarshanamohankumar-coder <nidarshanamohankumar@gmail.com>
2026-02-11 11:48:42 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$conn = pg_connect("
+host=ep-bitter-king-aiocqno2-pooler.c-4.us-east-1.aws.neon.tech
+port=5432
+dbname=neondb
+user=neondb_owner
+password=
+sslmode=require
+");
+
+if (!$conn) {
+    die("Database connection failed!");
+}
+?>
