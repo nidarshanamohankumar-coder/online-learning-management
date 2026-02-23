229c2fa4269617606344e735428d48c52ba64814
nidarshananidarshanamohankumar-coder <nidarshanamohankumar@gmail.com>
2026-02-23 11:39:23 +0530

    Initial commit - Online learning website

---
+++ full_stack_content.php
@@ -0,0 +1,173 @@
+<?php include("header.php");
+?>
+<link rel="stylesheet" href="full_stack_content.css">
+
+
+</style>
+
+<div class="bg">
+    <img src="images/full_stackk.png" alt="full stack">
+</div>
+
+<div class="course-content">
+<div class="content">
+    <h1 class="fadeUp">Course Content</h1>
+
+    <a href="pdf/Full _stack _plan.pdf" download class="download-btn">Download Brochure</a>
+</div>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 1 :</b>
+HTML Intro, Basics, Elements, Attributes, Paragraphs, Images, Favicon, Page Title, Tables, Lists</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 2 :</b>
+Forms (All Input Types), Semantic Tags, Page Structure, Mini Form Practice</h2>
+</div>
+
+
+<div class="box-content">
+    <h2><b>Day 3 :</b>
+CSS Basics, Selectors, Box Model, Margin, Padding, Borders</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 4 :</b>
+Flexbox, Positioning, Layout Building</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 5 :</b>
+CSS Grid, Responsive Design, Media Queries</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 6 :</b>
+Build Responsive Website (Header, Footer, Navigation, Layout)</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 7 :</b>
+Website Completion + Practice</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 8 :</b>
+JavaScript Basics, Variables, Operators, Conditionals</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 9 :</b>
+Loops (for, while), Functions, Scope Basics</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 10:</b>
+Arrays, Array Methods (map, filter, reduce), Objects</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 11 :</b>
+Strings, Numbers, Date, Math</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 12 :</b>
+DOM Basics, Selecting Elements, Changing Content & Styles</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 13 :</b>
+DOM Events, Event Listeners, Form Validation</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 14 :</b>
+Build Complete Form Validation Project</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 15 :</b>
+JSON (Parse, Stringify), Fetch Data</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 16 :</b>
+AJAX (XMLHttpRequest / Fetch), API Handling</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 17 :</b>
+Build Mini Project using DOM + AJAX</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 18 :</b>
+DBMS Concepts – ER Diagram, Normalization, Keys, Joins</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 19 :</b>
+Transactions, Indexing (Basic Concept), Interview DB Questions</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 20 :</b>
+MySQL – Create DB, Tables, Constraints</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 21 :</b>
+SELECT, WHERE, INSERT, UPDATE, DELETE</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 22 :</b>
+JOINS, GROUP BY, HAVING, Subqueries</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 23 :</b>
+Advanced Queries Practice + Real DB Problems</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 24 :</b>
+PHP Basics – Syntax, Variables, Operators, Control Statements</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 25 :</b>
+PHP Functions, Arrays, Superglobals
+</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 26 :</b>
+PHP Forms Handling & Validation</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 27 :</b>
+PHP + MySQL Connection (Insert & Fetch Data)</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 28 :</b>
+Sessions, Cookies, Authentication Basics</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 29 :</b>
+Build Login & Signup System with Password Hashing</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 30 :</b>
+Final Full Stack Project (Form + DB + Login + Data Display)</h2>
+</div>
+</div>
+</div>
+<?php include("footer.php");
+?>
---
+++ gen_ai.php
@@ -0,0 +1,216 @@
+<?php include("header.php");
+?>
+<link rel="stylesheet" href="gen_ai.css">
+
+
+</style>
+
+<div class="bg">
+    <img src="images/aii.png" alt="full stack">
+</div>
+
+<div class="course-content">
+<div class="content">
+    <h1 class="fadeUp">Course Content</h1>
+
+    <a href="pdf/30 Days Generative AI Roadmap.pdf" download class="download-btn">Download Brochure</a>
+</div>
+
+<h2 class="phase-title">PHASE 1 – Foundation (Days 1–10)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 1 :</b>
+What is AI? AI applications</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 2 :</b>
+Machine Learning vs Deep Learning
+</h2>
+</div>
+
+
+<div class="box-content">
+    <h2><b>Day 3 :</b>
+Supervised vs Unsupervised
+learning
+</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 4 :</b>
+Classification & Regression </h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 5 :</b>
+Overfitting & Underfitting</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 6 :</b>
+Accuracy, Precision, Recall, F1-
+score</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 7 :</b>
+NumPy basics</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 8 :</b>
+Pandas</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 9 :</b>
+Matplotlib</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 10:</b>
+Scikit-learn </h2>
+</div>
+</div>
+
+
+<h2 class="phase-title">PHASE 2 – Deep Learning (Days 11–14)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 11 :</b>
+Perceptron</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 12 :</b>
+Backpropagation</h2>
+</div>
+
+
+<div class="box-content">
+    <h2><b>Day 13 :</b>
+ReLU, Sigmoid, Softmax</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 14 :</b>
+PyTorch</h2>
+</div>
+</div>
+
+
+<h2 class="phase-title">PHASE 3 – NLP (Days 15–19)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 15 :</b>
+Text preprocessing</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 16 :</b>
+Word & sentence
+tokenization
+</h2>
+</div>
+
+
+<div class="box-content">
+    <h2><b>Day 17 :</b>
+Word2Vec, GloVe
+</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 18 :</b>
+RNN & LSTM 
+</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 19 :</b>
+Attention mechanism
+</h2>
+</div>
+</div>
+
+<h2 class="phase-title">PHASE 4 – Transformers & LLMs (Days 20–23)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 20 :</b>
+Transformer architecture</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 21 :</b>
+Self-Encoder vs Decoder
+</h2>
+</div>
+
+
+<div class="box-content">
+    <h2><b>Day 22 :</b>
+GPT architecture
+</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 23:</b>
+BERT architecture
+</h2>
+</div>
+</div>
+
+<h2 class="phase-title">PHASE 5 – Prompt Engineering (Days 24–25)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 24 :</b>
+Zero-shot, Few-shot, Role
+prompting</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 25 :</b>
+Chain-of-thought, Temperature
+control</h2>
+</div>
+</div>
+
+<h2 class="phase-title">PHASE 6 – GenAI APIs (Days 26–27)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 26 :</b>
+OpenAI, Gemini,
+HuggingFace</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 27 :</b>
+Connect API to UI</h2>
+</div>
+</div>
+
+<h2 class="phase-title">PHASE 7 – RAG (Days 28–29)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 28 :</b>
+FAISS / Pinecone</h2>
+</div>
+
+<div class="box-content">
+    <h2><b>Day 29 :</b>
+PDF → Q&A bot</h2>
+</div>
+</div>
+
+
+<h2 class="phase-title">PHASE 6 – GenAI APIs (Days 26–27)</h2>
+<div class="card-container">
+<div class="box-content">
+    <h2><b>Day 30 :</b>
+LoRA, PEFT, Custom training</h2>
+</div>
+</div>
+</div>
+<?php include("footer.php");
+?>
